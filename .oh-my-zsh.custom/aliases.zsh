4ff26bf834416aa09f9f7c4ecbe2fc7ec0c7a6ac
Erik Abi-Khattar <erik.abikhattar@gopuff.com>
2020-03-22 23:57:01 -0400

    bash and zsh config

---
+++ .oh-my-zsh.custom/aliases.zsh
@@ -0,0 +1,8 @@
+alias zshconfig="code ~/.zshrc"
+alias ohmyzsh="code ~/.oh-my-zsh"
+alias gitconfig='code ~/.gitconfig'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ls='ls -GwF'
+alias ll='ls -alh'
+alias cfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
---
+++ .oh-my-zsh.custom/golang.zsh
@@ -0,0 +1,4 @@
+export GOPATH=$(go env GOPATH)
+export GOROOT=$(go env GOROOT)
+export GOPRIVATE=github.com/nextmv-io/*
+export PATH=$PATH:$GOPATH/bin
