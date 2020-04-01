2c6417bb61a2c38e72b2fe85082ff345ea132a93
Erik Abi-Khattar <erik.abikhattar@gopuff.com>
2020-04-01 10:41:54 -0400

    base gopuff off master

--- .oh-my-zsh.custom/golang.zsh
+++ .oh-my-zsh.custom/golang.zsh
@@ -1,6 +1,7 @@
 if type go &> /dev/null; then
     export GOPATH=$HOME/.go
     export GOROOT="$(brew --prefix golang)/libexec"
+    export GOPRIVATE=github.com/gopuff/*,github.com/nextmv-io/*
     export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
     test -d "${GOPATH}" || mkdir "${GOPATH}"
     test -d "${GOPATH}/src/github.com" || mkdir -p "${GOPATH}/src/github.com"
